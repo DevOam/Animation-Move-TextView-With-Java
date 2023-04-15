d5f318ea41ea765b5300df5204c5fb211c02e9d9
DevOam <LAKOUAS-MOHAMEDREDA@ofppt-edu.ma>
2023-04-15 14:51:58 +0000

    Animation-move TextView-With-Java

---
+++ app/src/main/java/com/example/animation_move_textview_with_java/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.animation_move_textview_with_java;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    Animation anim;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text=findViewById(R.id.textView);
+        anim= AnimationUtils.loadAnimation(this,R.anim.move);
+
+        text.setAnimation(anim);
+
+    }
+}
